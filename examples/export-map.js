4d5b17aad70bc03af6ebe1e5d12aa81e5399a653
Frederic Junod <frederic.junod@camptocamp.com>
2013-05-28 10:54:43 +0200

    Snapshot

--- examples/export-map.js
+++ examples/export-map.js
@@ -28,3 +28,20 @@ var exportPNGElement = document.getElementById('export-png');
 exportPNGElement.addEventListener('click', function(e) {
   e.target.href = map.getRenderer().getCanvas().toDataURL('image/png');
 }, false);
+
+var exportPDFElement = document.getElementById('export-pdf');
+exportPDFElement.addEventListener('click', function(e) {
+  // FIXME: add attributions and logos
+  // FIXME: map size
+  var img = map.getRenderer().getCanvas().toDataURL('image/jpeg');
+  var doc = new jsPDF('landscape', 'mm', 'a4');
+
+  doc.addImage(img, 'jpeg', 10, 10);
+
+  doc.setFont('courier');
+  doc.setFontType('normal');
+  doc.text(10, 200, "FIXME: map.get('attributions').join(', ')");
+
+  e.target.href = doc.output('datauristring');
+});
+
