762e5a9f4e5550af98404dc32e9482dafaeb7946
Frederic Junod <frederic.junod@camptocamp.com>
2013-11-25 11:49:48 +0100

    Add text style default values

--- src/ol/render/canvas/canvas.js
+++ src/ol/render/canvas/canvas.js
@@ -10,9 +10,27 @@ ol.render.canvas.defaultFillStyle = ol.color.fromString('black');
 
 
 /**
+ * @const {string}
+ */
+ol.render.canvas.defaultFont = '10px sans-serif';
+
+
+/**
  * @const {ol.Color}
  */
 ol.render.canvas.defaultStrokeStyle = ol.color.fromString('black');
+
+
+/**
+ * @const {string}
+ */
+ol.render.canvas.defaultTextAlign = 'start';
+
+
+/**
+ * @const {string}
+ */
+ol.render.canvas.defaultTextBaseline = 'alphabetic';
 
 
 /**
