c2b66b7734cebe050a85d5dd8f8ce1ff0369af6d
Éric Lemoine <eric.lemoine@gmail.com>
2014-06-04 14:58:26 +0200

    Add a graticule example

---
+++ examples/graticule.js
@@ -0,0 +1,27 @@
+goog.require('ol.Graticule');
+goog.require('ol.Map');
+goog.require('ol.View2D');
+goog.require('ol.layer.Tile');
+goog.require('ol.proj');
+goog.require('ol.source.OSM');
+
+
+var map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  renderer: 'canvas',
+  target: 'map',
+  view: new ol.View2D({
+    center: ol.proj.transform([4.8, 47.75], 'EPSG:4326', 'EPSG:3857'),
+    zoom: 5
+  })
+});
+
+// Create the graticule component
+var graticule = new ol.Graticule({
+  projection: map.getView().getProjection()
+});
+graticule.setMap(map);
