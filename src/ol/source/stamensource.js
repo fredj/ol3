b6aee9013cae377d1725c7b6055a5f1f7269a1e1
Frederic Junod <frederic.junod@camptocamp.com>
2015-04-16 16:43:19 +0200

    Remove ol.IS_HTTPS and always request tiles with HTTPS

--- src/ol/source/stamensource.js
+++ src/ol/source/stamensource.js
@@ -99,11 +99,9 @@ ol.source.Stamen = function(options) {
       'known layer configured');
   var layerConfig = ol.source.StamenLayerConfig[options.layer];
 
-  var root = ol.IS_HTTPS ? 'https://stamen-tiles-{a-d}.a.ssl.fastly.net/' :
-      'http://{a-d}.tile.stamen.com/';
   var url = goog.isDef(options.url) ? options.url :
-      root + options.layer + '/{z}/{x}/{y}.' +
-      layerConfig.extension;
+      'https://stamen-tiles-{a-d}.a.ssl.fastly.net/' + options.layer +
+      '/{z}/{x}/{y}.' + layerConfig.extension;
 
   goog.base(this, {
     attributions: ol.source.Stamen.ATTRIBUTIONS,
