bdb5d8fcf7d44f8a00b5cc3421fba851a34f2874
Frederic Junod <frederic.junod@camptocamp.com>
2016-03-16 16:07:05 +0100

    snapshot

--- examples/flight-animation.js
+++ examples/flight-animation.js
@@ -1,17 +1,18 @@
 import Feature from '../src/ol/Feature.js';
 import Map from '../src/ol/Map.js';
 import View from '../src/ol/View.js';
-import LineString from '../src/ol/geom/LineString.js';
+import {Circle, LineString} from '../src/ol/geom.js';
 import {Tile as TileLayer, Vector as VectorLayer} from '../src/ol/layer.js';
-import Stamen from '../src/ol/source/Stamen.js';
+import TileJSON from '../src/ol/source/TileJSON.js';
 import VectorSource from '../src/ol/source/Vector.js';
-import {Stroke, Style} from '../src/ol/style.js';
+import {Fill, Stroke, Style} from '../src/ol/style.js';
+import {get as getProjection} from '../src/ol/proj.js';
 
 const map = new Map({
   layers: [
     new TileLayer({
-      source: new Stamen({
-        layer: 'toner'
+      source: new TileJSON({
+        url: 'https://api.tiles.mapbox.com/v3/mapbox.world-dark.json'
       })
     })
   ],
@@ -24,18 +25,62 @@ const map = new Map({
 
 const style = new Style({
   stroke: new Stroke({
-    color: '#EAE911',
-    width: 2
+    color: 'rgba(20, 77, 148, 0.6)',
+    width: 3
+  }),
+  fill: new Fill({
+    color: 'rgba(20, 77, 148, 0.6)'
   })
 });
 
+function addLater(feature, timeout) {
+  window.setTimeout(function() {
+    feature.set('start', new Date().getTime());
+    flightsSource.addFeature(feature);
+  }, timeout);
+}
+
+const pointsPerMs = 0.01;
+function animateFlights(event) {
+  const vectorContext = event.vectorContext;
+  const frameState = event.frameState;
+  vectorContext.setStyle(style);
+
+  event.context.shadowColor = '#95c6fe';
+  event.context.shadowBlur = 10;
+  event.context.globalCompositeOperation = 'lighter';
+  const features = flightsSource.getFeatures();
+  for (let i = 0; i < features.length; i++) {
+    const feature = features[i];
+    const coords = feature.getGeometry().getCoordinates();
+    const elapsedTime = frameState.time - feature.get('start');
+    const elapsedPoints = Math.floor(elapsedTime * pointsPerMs);
+
+    if (elapsedPoints >= coords.length) {
+      feature.set('finished', true);
+    }
+
+    coords.length = Math.min(elapsedPoints, coords.length);
+    const currentLine = new LineString(coords);
+
+    // directly draw the line with the vector context
+    vectorContext.drawGeometry(currentLine);
+    if (feature.get('finished')) {
+      const coordinate = feature.getGeometry().getLastCoordinate();
+      vectorContext.drawGeometry(new Circle(coordinate, 300000));
+    }
+  }
+  event.context.shadowBlur = 0;
+  event.context.globalCompositeOperation = 'source-over';
+  // tell OpenLayers to continue the animation
+  map.render();
+};
+
 const flightsSource = new VectorSource({
   wrapX: false,
-  attributions: 'Flight data by ' +
-        '<a href="http://openflights.org/data.html">OpenFlights</a>,',
+  attributions: 'Flight data by <a href="http://openflights.org/data.html">OpenFlights</a>,',
   loader: function() {
-    const url = 'data/openflights/flights.json';
-    fetch(url).then(function(response) {
+    fetch('data/openflights/flights.json').then(function(response) {
       return response.json();
     }).then(function(json) {
       const flightsData = json.flights;
@@ -47,12 +92,13 @@ const flightsSource = new VectorSource({
         // create an arc circle between the two locations
         const arcGenerator = new arc.GreatCircle(
           {x: from[1], y: from[0]},
-          {x: to[1], y: to[0]});
+          {x: to[1], y: to[0]}
+        );
 
         const arcLine = arcGenerator.Arc(100, {offset: 10});
         if (arcLine.geometries.length === 1) {
           const line = new LineString(arcLine.geometries[0].coords);
-          line.transform('EPSG:4326', 'EPSG:3857');
+          line.transform(getProjection('EPSG:4326'), getProjection('EPSG:3857'));
 
           const feature = new Feature({
             geometry: line,
@@ -70,52 +116,6 @@ const flightsSource = new VectorSource({
 
 const flightsLayer = new VectorLayer({
   source: flightsSource,
-  style: function(feature) {
-    // if the animation is still active for a feature, do not
-    // render the feature with the layer style
-    if (feature.get('finished')) {
-      return style;
-    } else {
-      return null;
-    }
-  }
+  style: null
 });
-
 map.addLayer(flightsLayer);
-
-const pointsPerMs = 0.1;
-function animateFlights(event) {
-  const vectorContext = event.vectorContext;
-  const frameState = event.frameState;
-  vectorContext.setStyle(style);
-
-  const features = flightsSource.getFeatures();
-  for (let i = 0; i < features.length; i++) {
-    const feature = features[i];
-    if (!feature.get('finished')) {
-      // only draw the lines for which the animation has not finished yet
-      const coords = feature.getGeometry().getCoordinates();
-      const elapsedTime = frameState.time - feature.get('start');
-      const elapsedPoints = elapsedTime * pointsPerMs;
-
-      if (elapsedPoints >= coords.length) {
-        feature.set('finished', true);
-      }
-
-      const maxIndex = Math.min(elapsedPoints, coords.length);
-      const currentLine = new LineString(coords.slice(0, maxIndex));
-
-      // directly draw the line with the vector context
-      vectorContext.drawGeometry(currentLine);
-    }
-  }
-  // tell OpenLayers to continue the animation
-  map.render();
-}
-
-function addLater(feature, timeout) {
-  window.setTimeout(function() {
-    feature.set('start', new Date().getTime());
-    flightsSource.addFeature(feature);
-  }, timeout);
-}
