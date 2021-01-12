fe9262286b22eb581167e2d66e2a21ebf79bf041
Frederic Junod <frederic.junod@camptocamp.com>
2021-01-12 15:06:02 +0100

    WIP Inner stroke style

--- examples/vector-layer.js
+++ examples/vector-layer.js
@@ -1,43 +1,56 @@
 import GeoJSON from '../src/ol/format/GeoJSON.js';
 import Map from '../src/ol/Map.js';
+import OSM from '../src/ol/source/OSM.js';
+import TileLayer from '../src/ol/layer/Tile.js';
 import VectorLayer from '../src/ol/layer/Vector.js';
 import VectorSource from '../src/ol/source/Vector.js';
 import View from '../src/ol/View.js';
-import {Fill, Stroke, Style, Text} from '../src/ol/style.js';
+import XYZ from '../src/ol/source/XYZ.js';
+import {Stroke, Style} from '../src/ol/style.js';
+import {getVectorContext} from '../src/ol/render.js';
 
-const style = new Style({
-  fill: new Fill({
-    color: 'rgba(255, 255, 255, 0.6)',
-  }),
+const strokeStyle = new Style({
   stroke: new Stroke({
-    color: '#319FD3',
-    width: 1,
-  }),
-  text: new Text({
-    font: '12px Calibri,sans-serif',
-    fill: new Fill({
-      color: '#000',
-    }),
-    stroke: new Stroke({
-      color: '#fff',
-      width: 3,
-    }),
+    color: 'rgba(0, 159, 128, 1)',
+    width: 2,
   }),
 });
 
-const vectorLayer = new VectorLayer({
+const innerStrokenStyle = new Style({
+  stroke: new Stroke({
+    color: 'rgba(0, 159, 128, 0.5)',
+    width: 22,
+    lineJoin: 'bevel',
+  }),
+});
+
+const countriesLayer = new VectorLayer({
   source: new VectorSource({
     url: 'data/geojson/countries.geojson',
     format: new GeoJSON(),
   }),
-  style: function (feature) {
-    style.getText().setText(feature.get('name'));
-    return style;
-  },
+  style: strokeStyle,
+});
+
+const innerStrokeLayer = new TileLayer({
+  source: new XYZ(),
+});
+
+const osm = new TileLayer({
+  source: new OSM(),
+});
+
+innerStrokeLayer.on('postrender', function (event) {
+  const vectorContext = getVectorContext(event);
+  countriesLayer.getSource().forEachFeature(feature => {
+    setClippingGeometry(vectorContext, feature.getGeometry());
+    vectorContext.drawFeature(feature, innerStrokenStyle);
+    resetClipping(vectorContext);
+  });
 });
 
 const map = new Map({
-  layers: [vectorLayer],
+  layers: [osm, innerStrokeLayer, countriesLayer],
   target: 'map',
   view: new View({
     center: [0, 0],
@@ -45,67 +58,35 @@ const map = new Map({
   }),
 });
 
-const highlightStyle = new Style({
-  stroke: new Stroke({
-    color: '#f00',
-    width: 1,
-  }),
-  fill: new Fill({
-    color: 'rgba(255,0,0,0.1)',
-  }),
-  text: new Text({
-    font: '12px Calibri,sans-serif',
-    fill: new Fill({
-      color: '#000',
-    }),
-    stroke: new Stroke({
-      color: '#f00',
-      width: 3,
-    }),
-  }),
-});
+function setClippingGeometry(vectorContext, geometry) {
+  const context = vectorContext.context_;
+  const type = geometry.getType();
+  const flatCoordinates = geometry.getOrientedFlatCoordinates();
+  const stride = geometry.getStride();
 
-const featureOverlay = new VectorLayer({
-  source: new VectorSource(),
-  map: map,
-  style: function (feature) {
-    highlightStyle.getText().setText(feature.get('name'));
-    return highlightStyle;
-  },
-});
-
-let highlight;
-const displayFeatureInfo = function (pixel) {
-  const feature = map.forEachFeatureAtPixel(pixel, function (feature) {
-    return feature;
-  });
-
-  const info = document.getElementById('info');
-  if (feature) {
-    info.innerHTML = feature.getId() + ': ' + feature.get('name');
+  context.save();
+  context.beginPath();
+  if (type === 'Polygon') {
+    const ends = geometry.getEnds();
+    for (let i = 0, ii = ends.length; i < ii; ++i) {
+      vectorContext.moveToLineTo_(flatCoordinates, 0, ends[i], stride, true);
+    }
+  } else if (type === 'MultiPolygon') {
+    let offset = 0;
+    const endss = geometry.getEndss();
+    for (let i = 0, ii = endss.length; i < ii; ++i) {
+      const ends = endss[i];
+      offset = vectorContext.drawRings_(flatCoordinates, offset, ends, stride);
+    }
   } else {
-    info.innerHTML = '&nbsp;';
+    console.error('Unsupported geometry type', type);
   }
+  context.clip();
+}
 
-  if (feature !== highlight) {
-    if (highlight) {
-      featureOverlay.getSource().removeFeature(highlight);
-    }
-    if (feature) {
-      featureOverlay.getSource().addFeature(feature);
-    }
-    highlight = feature;
-  }
-};
-
-map.on('pointermove', function (evt) {
-  if (evt.dragging) {
-    return;
-  }
-  const pixel = map.getEventPixel(evt.originalEvent);
-  displayFeatureInfo(pixel);
-});
-
-map.on('click', function (evt) {
-  displayFeatureInfo(evt.pixel);
-});
+function resetClipping(vectorContext) {
+  vectorContext.context_.restore();
+  // the save/restore mess with fill and stroke state.
+  vectorContext.contextStrokeState_ = null;
+  vectorContext.contextFillState_ = null;
+}
